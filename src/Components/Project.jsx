2064a14edc83d866392e7ff7815ba3edfead1dbd
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2021-02-15 22:54:10 -0600

    updated skill set

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -4,8 +4,8 @@ import javaimg from '../Components/public/image/java.png'
 import jsimg from '../Components/public/image/javascriptimg2.png'
 import htmlimg from '../Components/public/image/html5logo.jpg'
 import cssimg from '../Components/public/image/csslogo.png'
-import mongoimg from '../Components/public/image/mngodbimg.png'
-import databasebimg from '../Components/public/image/database.jpg'
+import reactlogo from '../Components/public/image/logo192.png'
+import cucumber from '../Components/public/image/cucumber.png'
 
 
 
@@ -21,8 +21,8 @@ class Project extends Component {
                     <div><img src={jsimg} width="100" height="100" alt="js"/></div>
                     <div><img src={htmlimg} width="100" height="100" alt="html"/></div>  
                     <div><img src={cssimg} width="100" height="100" alt="css"/></div>
-                    <div><img src={mongoimg} width="100" height="100" alt="mongodb"/></div>  
-                    <div><img src={databasebimg} width="100" height="100" alt="db"/></div>
+                    <div><img src={reactlogo} width="100" height="100" alt="reactlogo"/></div>  
+                    <div><img src={cucumber} width="100" height="100" alt="cucumber"/></div>
                 
             </section>
         )
