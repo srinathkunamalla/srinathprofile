d034680fb0eac01a7a85d6f2c27bfbec77c265bb
Srinath Kunamalla <s0k022y@homeoffice.wal-mart.com>
2021-02-15 22:36:29 -0600

    Updates

---
+++ precache-manifest.1e6598b8c739de66d3310f8414370e58.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864ca0bdb2aa2d70def8ebe22136b9de",
+    "url": "/srinathprofile/index.html"
+  },
+  {
+    "revision": "111819ba940afa3f21fe",
+    "url": "/srinathprofile/static/css/main.79b28cce.chunk.css"
+  },
+  {
+    "revision": "ffee68174c1cea8a65b0",
+    "url": "/srinathprofile/static/js/2.815c0981.chunk.js"
+  },
+  {
+    "revision": "111819ba940afa3f21fe",
+    "url": "/srinathprofile/static/js/main.a157609e.chunk.js"
+  },
+  {
+    "revision": "d8e2b9f923e825234d0f",
+    "url": "/srinathprofile/static/js/runtime~main.2b4bc3e0.js"
+  },
+  {
+    "revision": "eb0d38e2843d3a1025a6e30b84934c85",
+    "url": "/srinathprofile/static/media/biopicimg.eb0d38e2.jpg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/srinathprofile/static/media/burgerlogo.b8503d26.png"
+  },
+  {
+    "revision": "e38766a187961322358125fe8517de2d",
+    "url": "/srinathprofile/static/media/contactlogo.e38766a1.png"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/srinathprofile/static/media/csslogo.04cf122b.png"
+  },
+  {
+    "revision": "719fefb4c89c646aedd5bb462af9d8ff",
+    "url": "/srinathprofile/static/media/database.719fefb4.jpg"
+  },
+  {
+    "revision": "36ade360bd7ece317bfe98ed13757cba",
+    "url": "/srinathprofile/static/media/forbidden.36ade360.png"
+  },
+  {
+    "revision": "3a431a93c33adf4f325cfbd1547c8149",
+    "url": "/srinathprofile/static/media/html5logo.3a431a93.jpg"
+  },
+  {
+    "revision": "184a7681dfc7ae09341edaafc979da8f",
+    "url": "/srinathprofile/static/media/javascriptimg2.184a7681.png"
+  },
+  {
+    "revision": "d261a13e651f8cdc82a378b29f155537",
+    "url": "/srinathprofile/static/media/mngodbimg.d261a13e.png"
+  },
+  {
+    "revision": "a94a2440970098421a255a63a9f81aeb",
+    "url": "/srinathprofile/static/media/sk.a94a2440.png"
+  },
+  {
+    "revision": "deb493608dae9e533ef6b6894095b797",
+    "url": "/srinathprofile/static/media/yt1.deb49360.png"
+  }
+]);
